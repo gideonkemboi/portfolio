cc66ab0ed3837be8ea2a2c23019548a95b7a912d
Gideon Kemboi <gedionkemboi18@gmail.com>
2024-08-01 13:12:56 +0300

    Implement routing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,27 @@
-function App() {
-  return <>Home</>;
-}
+import { Helmet } from "react-helmet";
+import Hero from "./hero/Hero";
+import About from "./about/About";
+import Projects from "./portfolio/Portfolio";
+import Contact from "./contact/Contact";
+
+const App = () => {
+  return (
+    <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Gideon Kemboi | Fullstack Developer</title>
+        <meta
+          name="description"
+          content="Personal portfolio website for Gideon Kemboi with his bio, projects and contact information"
+        />
+        <html lang="en" />
+      </Helmet>
+      <Hero />
+      <About />
+      <Projects />
+      <Contact />
+    </div>
+  );
+};
 
 export default App;
